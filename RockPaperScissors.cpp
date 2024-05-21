5df9716800f7580a7fc7b8727fa9fd229dc1f106
Shin <128938362+shinkurohoshi@users.noreply.github.com>
2024-05-21 05:06:54 -0800

    Update RockPaperScissors.cpp

--- RockPaperScissors.cpp
+++ RockPaperScissors.cpp
@@ -1,88 +1,74 @@
 #include <iostream>
 #include <ctime>
+#include <unordered_map>
+#include <string>
 
-using std::cout; using std::endl; using std::cin;
+using namespace std;
 
-char PlayerChoice();
-char OpponentChoice();
-void getChoice(char choice);
-void PlayResults(char player, char opponent);
+unordered_map<string, int> hands{
+    {"rock", 1},
+    {"paper", 2},
+    {"scissors", 3}
+};
 
+string getCompChoice(int compChoice) {
+    switch (compChoice) {
+    case 1:
+        return "rock";
+    case 2:
+        return "paper";
+    case 3:
+        return "scissors";
+    default:
+        return "";
+    }
+}
 
-int main(){
-    char player;
-    char opponent;
-    char playAgain;
+int comp() {
+    srand(time(NULL));
+    int ran = rand() % 3 + 1;
+    return ran;
+}
 
-    do{
-    player = PlayerChoice();
-    cout<< "Player Choice:";
-    getChoice(player);
+bool checkWin(int userChoice, int compChoice) {
+    if (userChoice == compChoice) {
+        cout << "It's a tie!\n";
+        return false;
+    }
+    if ((userChoice == 1 && compChoice == 3) ||
+        (userChoice == 2 && compChoice == 1) ||
+        (userChoice == 3 && compChoice == 2)) {
+        cout << "You win!\n";
+        return true;
+    }
+    else {
+        cout << "You lose!\n";
+        return false;
+    }
+}
 
-    opponent = OpponentChoice();
-    cout<< "\nOpponent's Choice:";
-    getChoice(opponent);
-   
-    cout<< "\nResults: ";
-    PlayResults(player, opponent);
+int main() {
+    string user;
+    bool victory = false;
 
-    cout<< "\nDo you want to continue?: (y/n)"<< endl;
-    cin>> playAgain;
-    }while(playAgain != 'n');
-    
+    while (!victory) {
+        cout << "Rock-Paper-Scissors: ";
+        cin >> user;
+
+        if (hands.find(user) == hands.end()) {
+            cout << "Invalid input. Please enter rock, paper, or scissors.\n";
+            continue;
+        }
+
+        int userChoice = hands[user];
+        int compChoice = comp();
+        string compChoiceStr = getCompChoice(compChoice);
+
+        cout << "You chose: " << user << endl;
+        cout << "Computer chose: " << compChoiceStr << endl;
+
+        victory = checkWin(userChoice, compChoice);
+    }
 
     return 0;
-}
-
-char PlayerChoice(){
-    char player;
-
-    do{
-    cout<<"\n******************************"<<endl;
-    cout<<"\nEnter your action (Rock =r, Paper = p, Scissors = s)"<<endl;
-    cout<<"\n******************************"<<endl;
-    cin>>player;
-
-    }while(player != 'r' && player != 'p' && player != 's');
-    
-    return player;
-}
-char OpponentChoice(){
-    srand(time(NULL));
-    int comp = rand() % 3 + 1;
-
-    switch (comp)
-    {
-    case 1: return 'r'; 
-    case 2: return 'p'; 
-    case 3: return 's';
-    }
-    return comp;
-}
-void getChoice(char choice){
-    switch (choice)
-    {
-    case 'r':cout<< " Rock"; break;
-    case 'p':cout<< " Paper"; break;
-    case 's':cout<< " Scissors"; break;
-    }
-
-}
-void PlayResults(char player, char opponent){
-    switch (player)
-    {
-    case 'r': if (opponent=='r'){cout<< "You are tied";}
-              else if (opponent=='p'){cout<< "You lost";}
-              else {cout<< "You won";}
-              break;
-    case 'p': if (opponent=='r'){cout<< "You won";}
-              else if (opponent=='p'){cout<< "You are tied";}
-              else {cout<< "You lost";}
-              break;
-    case 's': if (opponent=='r'){cout<< "You lost";}
-              else if (opponent=='p'){cout<< "You won";}
-              else {cout<< "You are tied";}
-              break;
-  
-    }
 }
